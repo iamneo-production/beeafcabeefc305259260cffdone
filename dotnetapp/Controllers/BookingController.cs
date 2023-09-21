d89ad34327ef2147d98e607ed77b3d5de3ba1f77
Iamneo <techadmin@iamneo.ai>
2023-09-21 08:09:36 +0000

    Backup Commit

--- dotnetapp/Controllers/BookingController.cs
+++ dotnetapp/Controllers/BookingController.cs
@@ -1,15 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 // using gym.Data;
+using gymI.Exceptions; 
 
-using dotnetapp.Models;
+using gymI.Models;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace dotnetapp.Controllers
+namespace gymI.Controllers
 {
-    
+
+
 
 
 public class BookingController : Controller
@@ -21,6 +23,67 @@ public class BookingController : Controller
         _context = context;
     }
 
+    public IActionResult Index()
+    {
+        var bookings = _context.Bookings.Include(b => b.Slot).ToList();
+        return View(bookings);
+    }
+
+    public IActionResult Book(int id)
+    {
+        var slot = _context.Slots.FirstOrDefault(s => s.SlotID == id);
+
+        if (slot == null)
+        {
+            return NotFound();
+        }
+
+        return View(slot);
+    }
+
+    [HttpPost]
+public IActionResult Book(int id, int userId)
+{
+    var slot = _context.Slots.FirstOrDefault(s => s.SlotID == id);
+
+    if (slot == null)
+    {
+        return NotFound();
+    }
+
+    if (slot.Capacity <= 0)
+    {
+        throw new SlotBookingException("Slot is full.");
+    }
+
+    var existingBooking = _context.Bookings.FirstOrDefault(b => b.SlotID == id && b.UserID == userId);
+
+    if (existingBooking != null)
+    {
+        throw new SlotBookingException("You have already booked this slot.");
+    }
+
+    else{
+
+    var booking = new Booking
+    {
+        SlotID = id,
+        UserID = userId
+    };
+
+    _context.Bookings.Add(booking);
+    slot.Capacity--;
+    var res=_context.SaveChanges();
+    if (res > 0)
+    { return RedirectToAction("Index");
+    }   
+    }
+         return RedirectToAction("Book");
 
 }
+
+}
+
+
+
 }
