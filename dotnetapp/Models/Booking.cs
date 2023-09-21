60164933ec740371aa585d1c170cfd1d965f2dca
Iamneo <techadmin@iamneo.ai>
2023-09-21 08:14:47 +0000

    Backup Commit

--- dotnetapp/Models/Booking.cs
+++ dotnetapp/Models/Booking.cs
@@ -6,8 +6,12 @@ using System.ComponentModel.DataAnnotations;
 namespace dotnetapp.Models
 
 {
-
 public class Booking
 {
+    public int BookingID { get; set; }
+    public int SlotID { get; set; }
+    public int UserID { get; set; }
+    public Slot Slot { get; set; }
+}
 
-}}
+}
--- dotnetapp/Models/Slot.cs
+++ dotnetapp/Models/Slot.cs
@@ -9,5 +9,10 @@ namespace dotnetapp.Models
 
 public class Slot
 {
-
-}}
+    public int SlotID { get; set; }
+    public DateTime Time { get; set; }
+    public int Duration { get; set; }
+    public int Capacity { get; set; }
+    public ICollection<Booking> Bookings { get; set; }
+}
+}
